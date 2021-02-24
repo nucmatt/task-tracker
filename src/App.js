316166adec9fd6c426c1f77642e351707cdf451d
Matthew Wessel <nucmatt@gmail.com>
2021-02-23 20:11:09 -0600

    Build: Create Header component
    Creates a components folder and adds a Header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="container">
-     <h1>Hello from React</h1>
-     <h2>Hello</h2>
+     <Header />
     </div>
   );
 }
