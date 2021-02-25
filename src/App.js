2044284f0d9283863da8c8955d1fc13b4306ef19
Matthew Wessel <nucmatt@gmail.com>
2021-02-25 10:46:59 -0600

    Build: Add deleteTask event
    Adds the deleteTask event and passes it down to the Task component to
    allow for deleting individual tasks via clicking on the FaTimes X within
    each task.

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ function App() {
         reminder: false
     },
 ])
+
+// Delete Task
+const deleteTask = (id) => {
+  // filter tasks to exclude the deleted task via it's id(passed in from Task component onClick event)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks}/>
+     <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
