e289ed089c492bff2ef02d19e9b67c758ae1891e
Matthew Wessel <nucmatt@gmail.com>
2021-02-25 10:49:14 -0600

    Build: Add message for No Tasks if tasks length is 0
    Uses the ternary operator to add a message of 'No Tasks' to the UI if
    there are no tasks to display.

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const deleteTask = (id) => {
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks} onDelete={deleteTask}/>
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks'}
     </div>
   );
 }
