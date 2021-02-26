d13e422c6bbf6822b4b85b912b1dc1456a9f043f
Matthew Wessel <nucmatt@gmail.com>
2021-02-26 11:00:40 -0600

    Build: Create About component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
