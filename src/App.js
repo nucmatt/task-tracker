16700887a617af8e62aa15398a350ad8ae69586c
Matthew Wessel <nucmatt@gmail.com>
2021-02-26 10:35:09 -0600

    Build: Update addTask to use json-server
    addTask now updates the db.json file when adding a new task.

--- src/App.js
+++ src/App.js
@@ -25,10 +25,22 @@ const fetchTasks = async () => {
 };
 
 // Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000 + 1);
-  const newTask = { id, ...task };
-  setTasks([...tasks, newTask]);
+const addTask = async (task) => {
+// json-server automatically creates ids so below code is no longer needed
+  // const id = Math.floor(Math.random() * 10000 + 1);
+  // const newTask = { id, ...task };
+  // setTasks([...tasks, newTask]);
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    // convert task object to JSON
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json();
+  setTasks([...tasks, data]);
 }
 
 // Delete Task
