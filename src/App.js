96722b65b784cd2bbe1bf38b6e35b4662c7cbeac
Matthew Wessel <nucmatt@gmail.com>
2021-02-26 10:17:06 -0600

    Build: Delete tasks from both UI and db.json
    Moves tasks from App.js state to db.json file. Creates route to
    permanently delete tasks from db.json.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
-    },
-])
+  const [tasks, setTasks] = useState([]);
+
+  // Fetch saved tasks on page load
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+
+// Fetch tasks
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks');
+  const data = await res.json();
+
+  return data
+};
 
 // Add Task
 const addTask = (task) => {
@@ -34,7 +32,10 @@ const addTask = (task) => {
 }
 
 // Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
   // filter tasks to exclude the deleted task via it's id(passed in from Task component onClick event)
   setTasks(tasks.filter((task) => task.id !== id))
 }
