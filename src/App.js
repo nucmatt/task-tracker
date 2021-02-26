ea22677be18dddc288841750c3a8c6327f6ddb31
Matthew Wessel <nucmatt@gmail.com>
2021-02-26 10:46:05 -0600

    Build: Update toggleReminder to use json-server
    toggleReminder now toggles the task reminder in db.json for permanency.

--- src/App.js
+++ src/App.js
@@ -4,74 +4,96 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
+	const [showAddTask, setShowAddTask] = useState(false);
+	const [tasks, setTasks] = useState([]);
 
-  // Fetch saved tasks on page load
-  useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks();
-      setTasks(tasksFromServer);
-    }
-    getTasks();
-  }, []);
+	// Fetch saved tasks on page load
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer);
+		};
+		getTasks();
+	}, []);
 
-// Fetch tasks
-const fetchTasks = async () => {
-  const res = await fetch('http://localhost:5000/tasks');
-  const data = await res.json();
+	// Fetch tasks
+	const fetchTasks = async () => {
+		const res = await fetch('http://localhost:5000/tasks');
+		const data = await res.json();
 
-  return data
-};
+		return data;
+	};
 
-// Fetch single task
-const fetchTask = async (id) => {
-  const res = await fetch(`http://localhost:5000/tasks/${id}`);
-  const data = await res.json();
+	// Fetch single task
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`);
+		const data = await res.json();
 
-  return data
-};
+		return data;
+	};
 
-// Add Task
-const addTask = async (task) => {
-// json-server automatically creates ids so below code is no longer needed
-  // const id = Math.floor(Math.random() * 10000 + 1);
-  // const newTask = { id, ...task };
-  // setTasks([...tasks, newTask]);
-  const res = await fetch('http://localhost:5000/tasks', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    // convert task object to JSON
-    body: JSON.stringify(task)
-  })
+	// Add Task
+	const addTask = async (task) => {
+		// json-server automatically creates ids so below code is no longer needed
+		// const id = Math.floor(Math.random() * 10000 + 1);
+		// const newTask = { id, ...task };
+		// setTasks([...tasks, newTask]);
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			// convert task object to JSON
+			body: JSON.stringify(task),
+		});
 
-  const data = await res.json();
-  setTasks([...tasks, data]);
-}
+		const data = await res.json();
+		setTasks([...tasks, data]);
+	};
 
-// Delete Task
-const deleteTask = async (id) => {
-  await fetch(`http://localhost:5000/tasks/${id}`, {
-    method: 'DELETE'
-  })
-  // filter tasks to exclude the deleted task via it's id(passed in from Task component onClick event)
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+	// Delete Task
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE',
+		});
+		// filter tasks to exclude the deleted task via it's id(passed in from Task component onClick event)
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
 
-// Toggle Reminder
-const toggleReminder =(id) => {
-  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
-}
-  return (
-    <div className="container">
-     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-     {/* double ampersand is a shortform ternary operator if there is no 'else' to the ternary if*/}
-     {showAddTask && <AddTask onAdd={addTask} />}
-     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'}
-    </div>
-  );
+	// Toggle Reminder
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
+		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(updTask),
+		});
+		const data = await res.json();
+
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: data.reminder } : task
+			)
+		);
+	};
+	return (
+		<div className='container'>
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAdd={showAddTask}
+			/>
+			{/* double ampersand is a shortform ternary operator if there is no 'else' to the ternary if*/}
+			{showAddTask && <AddTask onAdd={addTask} />}
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+			) : (
+				'No Tasks'
+			)}
+		</div>
+	);
 }
 
 export default App;
